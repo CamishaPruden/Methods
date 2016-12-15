a466498416fd8fcd67667da142f02645ebc3bfa4
CamishaPruden <c.pruden@sjasd.ca>
2016-12-15 10:39:47 -0600

    Methods
    Displaying methods

---
+++ src/MethodCoding.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+
+/**
+ * @author c.pruden
+ *
+ */
+public class MethodCoding {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		displayName();
+		displayBirthday();
+		displayFact();
+		
+	}
+	
+	/**
+	 * displayName will display my name to the
+	 * user
+	 * 
+	 * @param There are no parameters.
+	 * @return Nothing will be returned.
+	 */
+	public static void displayName(){
+		
+		System.out.println("My name is Camisha.");
+		
+	}
+	
+	/**
+	 * displayBirthday will display my birthday to the
+	 * user
+	 * 
+	 * @param There are no parameters.
+	 * @return Nothing will be returned.
+	 */
+	public static void displayBirthday(){
+		
+		System.out.println("My birthday is August 28!");
+		
+	}
+	
+	/**
+	 * displayFact will display one fact about me to the
+	 * user
+	 * 
+	 * @param There are no parameters.
+	 * @return Nothing will be returned.
+	 */
+	public static void displayFact(){
+		
+		System.out.println("One fact about me is, winter is my favorite season!");
+		
+	}
+
+}
